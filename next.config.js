154e179e6c46b27564b9fcdd68044f12541a6a7d
sashika20643 <sashikadulaj20643@gmail.com>
2023-10-21 16:51:55 +0530

    next.config changed

--- next.config.js
+++ next.config.js
@@ -2,9 +2,8 @@
 const nextConfig = {
   swcMinify: false,
   reactStrictMode: true,
-  // Add this line to enable serving files from the static directory
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/your-nextjs-project' : '',
-};
+}
 
-module.exports = nextConfig;
 
+
+module.exports = nextConfig
